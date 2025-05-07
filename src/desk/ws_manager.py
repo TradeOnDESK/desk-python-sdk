90c49b7f83f2b3be200029fc072043c04549bc2c
chenciao8 <chenciao8@gmail.com>
2025-05-07 16:00:11 +0700

    fix: ws resubscribe when reconnect

--- src/desk/ws_manager.py
+++ src/desk/ws_manager.py
@@ -7,7 +7,9 @@ import threading
 
 from desk.types import Subscription, WsMessage
 
-ActiveSubscription = NamedTuple("ActiveSubscription", [("callback", Callable[[Any], None]), ("subscription_id", int)])
+ActiveSubscription = NamedTuple("ActiveSubscription", [(
+    "callback", Callable[[Any], None]), ("subscription_id", int)])
+
 
 class WebSocketManager(threading.Thread):
     def __init__(self, ws_url: str):
@@ -16,16 +18,19 @@ class WebSocketManager(threading.Thread):
         self.ws_ready = False
         self.url = ws_url
 
-        self.queued_subscriptions: List[Tuple[Subscription, ActiveSubscription]] = []
-        self.active_subscriptions: Dict[str, List[ActiveSubscription]] = defaultdict(list)
+        self.queued_subscriptions: List[Tuple[Subscription, ActiveSubscription]] = [
+        ]
+        self.active_subscriptions: Dict[str,
+                                        List[ActiveSubscription]] = defaultdict(list)
 
-        self.ws = WebSocketApp(ws_url, on_message=self.on_message, on_open=self.on_open)
+        self.ws = WebSocketApp(
+            ws_url, on_message=self.on_message, on_open=self.on_open, on_close=self.on_close)
         self.ping_sender = threading.Thread(target=self.send_ping)
         self.stop_event = threading.Event()
-    
+
     def run(self):
         self.ws.run_forever(suppress_origin=True, reconnect=1)
-    
+
     def send_ping(self):
         while not self.stop_event.wait(50):
             if not self.ws.keep_running:
@@ -50,7 +55,8 @@ class WebSocketManager(threading.Thread):
             return
         active_subscriptions = self.active_subscriptions[identifier]
         if len(active_subscriptions) == 0:
-            print("Websocket message from an unexpected subscription:", message, identifier)
+            print("Websocket message from an unexpected subscription:",
+                  message, identifier)
         else:
             for active_subscription in active_subscriptions:
                 active_subscription.callback(ws_msg)
@@ -58,8 +64,34 @@ class WebSocketManager(threading.Thread):
     def on_open(self, _ws):
         logging.debug("on_open")
         self.ws_ready = True
+
+        # Move all active subscriptions to queued subscriptions
+        for identifier, subscriptions in self.active_subscriptions.items():
+            for active_subscription in subscriptions:
+                # Find the original subscription from the active subscriptions
+                # Basic subscription with type
+                subscription = {"type": identifier}
+                self.queued_subscriptions.append(
+                    (subscription, active_subscription))
+                # Unsubscribe from the active subscription
+                self.unsubscribe(
+                    subscription, active_subscription.subscription_id)
+
+        # Clear active subscriptions
+        self.active_subscriptions.clear()
+
+        # Resubscribe everything from queued subscriptions
         for subscription, active_subscription in self.queued_subscriptions:
-            self.subscribe(subscription, active_subscription.callback, active_subscription.subscription_id)
+            self.subscribe(subscription, active_subscription.callback,
+                           active_subscription.subscription_id)
+
+        # Clear queued subscriptions
+        self.queued_subscriptions.clear()
+
+    def on_close(self, _ws, close_status_code, close_msg):
+        logging.debug(
+            f"WebSocket closed with status {close_status_code}: {close_msg}")
+        self.ws_ready = False
 
     def subscribe(
         self, subscription: Subscription, callback: Callable[[Any], None], subscription_id: Optional[int] = None
@@ -69,22 +101,28 @@ class WebSocketManager(threading.Thread):
             subscription_id = self.subscription_id_counter
         if not self.ws_ready:
             logging.debug("enqueueing subscription")
-            self.queued_subscriptions.append((subscription, ActiveSubscription(callback, subscription_id)))
+            self.queued_subscriptions.append(
+                (subscription, ActiveSubscription(callback, subscription_id)))
         else:
             logging.debug("subscribing")
             identifier = subscription['type']
-            self.active_subscriptions[identifier].append(ActiveSubscription(callback, subscription_id))
-            self.ws.send(json.dumps({"method": "subscribe", "subscription": subscription}))
-        
+            self.active_subscriptions[identifier].append(
+                ActiveSubscription(callback, subscription_id))
+            self.ws.send(json.dumps(
+                {"method": "subscribe", "subscription": subscription}))
+
         return subscription_id
 
     def unsubscribe(self, subscription: Subscription, subscription_id: int) -> bool:
         if not self.ws_ready:
-            raise NotImplementedError("Can't unsubscribe before websocket connected")
+            raise NotImplementedError(
+                "Can't unsubscribe before websocket connected")
         identifier = subscription['type']
         active_subscriptions = self.active_subscriptions[identifier]
-        new_active_subscriptions = [x for x in active_subscriptions if x.subscription_id != subscription_id]
+        new_active_subscriptions = [
+            x for x in active_subscriptions if x.subscription_id != subscription_id]
         if len(new_active_subscriptions) == 0:
-            self.ws.send(json.dumps({"method": "unsubscribe", "subscription": subscription}))
+            self.ws.send(json.dumps(
+                {"method": "unsubscribe", "subscription": subscription}))
         self.active_subscriptions[identifier] = new_active_subscriptions
         return len(active_subscriptions) != len(new_active_subscriptions)
